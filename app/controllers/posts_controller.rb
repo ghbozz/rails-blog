6d86f0ac0fec5408820f4bb7a9dbd6116f7d5cd3
romain sanson <romainsanson.gm@gmail.com>
2018-11-28 18:15:43 +0100

    front on post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,17 @@ class PostsController < ApplicationController
   def show
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.save
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
