03d041523e90ae6e94a612a3a9b09edcd3303906
romain sanson <romainsanson.gm@gmail.com>
2019-02-20 19:33:09 +0100

    comments tweak

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'posts#index'
 
   resources :posts do
-    resources :comments, only: [:new, :create]
+    resources :comments, only: [:create]
   end
 
   resources :comments, only: [:destroy]
