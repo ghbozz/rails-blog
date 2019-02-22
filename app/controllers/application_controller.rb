0fe92cf2078b6fc398eb8aa1f612cb926b2fa49f
romain sanson <romainsanson.gm@gmail.com>
2019-02-22 21:15:02 +0100

    User Edit Form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password) }
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password])
+  end
 end
