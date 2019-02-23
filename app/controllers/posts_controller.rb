c08d70359d0ee65f057726c08696e34e8d5291d6
romain sanson <romainsanson.gm@gmail.com>
2019-02-23 20:36:37 +0100

    search refacto

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,19 +3,7 @@ class PostsController < ApplicationController
 
   def index
     if params[:query]
-      if params[:query][:title].present? && params[:query][:user].present?
-        sql_query = "title ILIKE :query OR content ILIKE :query"
-        posts_tmp = Post.where(sql_query, query: "%#{params[:query][:title]}%")
-        @posts = posts_tmp.select { |post| post.user.email == params[:query][:user] }
-      elsif params[:query][:title].present?
-        sql_query = "title ILIKE :query OR content ILIKE :query"
-        @posts = Post.where(sql_query, query: "%#{params[:query][:title]}%")
-      elsif params[:query][:user].present?
-        posts_tmp = Post.all.order('created_at DESC')
-        @posts = posts_tmp.select { |post| post.user.email == params[:query][:user] }
-      else
-        @posts = Post.all.order('created_at DESC')
-      end
+      search(params[:query])
     else
       @posts = Post.all.order('created_at DESC')
     end
@@ -51,6 +39,22 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def search(query)
+    if query[:title].present? && query[:user].present?
+      sql_query = "title ILIKE :query OR content ILIKE :query"
+      posts_tmp = Post.where(sql_query, query: "%#{query[:title]}%")
+      @posts = posts_tmp.select { |post| post.user.email == query[:user] }
+    elsif query[:title].present?
+      sql_query = "title ILIKE :query OR content ILIKE :query"
+      @posts = Post.where(sql_query, query: "%#{query[:title]}%")
+    elsif query[:user].present?
+      posts_tmp = Post.all.order('created_at DESC')
+      @posts = posts_tmp.select { |post| post.user.email == query[:user] }
+    else
+      @posts = Post.all.order('created_at DESC')
+    end
+  end
 
   def post_params
     params.require(:post).permit(:title, :content, :photo)
