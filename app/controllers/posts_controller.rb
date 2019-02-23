67f7d08ea4ca705265eac6e076602386edab4ffd
romain sanson <romainsanson.gm@gmail.com>
2019-02-23 16:39:20 +0100

    remove raise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
 
   def index
     if params[:query]
-      raise
       sql_query = "title ILIKE :query OR content ILIKE :query"
       @posts = Post.where(sql_query, query: "%#{params[:query]}%")
     else
