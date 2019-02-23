b20f31e0fd17a02153394fe9944de3fe38985a88
romain sanson <romainsanson.gm@gmail.com>
2019-02-23 20:30:06 +0100

    search by title content & user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,19 @@ class PostsController < ApplicationController
 
   def index
     if params[:query]
-      sql_query = "title ILIKE :query OR content ILIKE :query"
-      @posts = Post.where(sql_query, query: "%#{params[:query]}%")
+      if params[:query][:title].present? && params[:query][:user].present?
+        sql_query = "title ILIKE :query OR content ILIKE :query"
+        posts_tmp = Post.where(sql_query, query: "%#{params[:query][:title]}%")
+        @posts = posts_tmp.select { |post| post.user.email == params[:query][:user] }
+      elsif params[:query][:title].present?
+        sql_query = "title ILIKE :query OR content ILIKE :query"
+        @posts = Post.where(sql_query, query: "%#{params[:query][:title]}%")
+      elsif params[:query][:user].present?
+        posts_tmp = Post.all
+        @posts = posts_tmp.select { |post| post.user.email == params[:query][:user] }
+      else
+        @posts = Post.all.order('created_at DESC')
+      end
     else
       @posts = Post.all.order('created_at DESC')
     end
